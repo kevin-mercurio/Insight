dcde42247c7bfb23f62279428608b15d405389cd
Kevin Mercurio <kevinmercurio@Kevin-Mercurios-MacBook-Pro.local>
2014-08-15 14:27:02 -0400

    demo day 3

--- app/views.py
+++ app/views.py
@@ -8,19 +8,28 @@ age1l = []
 numhsl = []
 SESl = []
 frdropl = []
-behavel = []
+absentl = []
+skipl = []
+suspl = []
+hhnuml = []
+dropprogl = []
+dummyl = []
 
 @app.route("/")
 def landing():
   return render_template("landing.html")
 
 @app.route("/index")
-def landing():
+def index():
   return render_template("landing.html")
 
 @app.route("/AddStudent")
 def home():
   return render_template("home2.html")
+
+@app.route("/contact")
+def contact():
+  return render_template("contact.html")
 
 @app.route("/clear")
 def homepage():
@@ -30,7 +39,12 @@ def homepage():
   numhsl = []
   SESl = []
   frdropl = []
-  behavel = []
+  absentl = []
+  skipl = []
+  suspl = []
+  hhnuml = []
+  dropprogl = []
+  dummyl = []
   return render_template("home2.html")
 
 @app.route("/result")
@@ -39,31 +53,66 @@ def resultpage():
   ID = float(request.args.get('ID'))
   math2 = float(request.args.get('math2'))
   age1 = int(request.args.get('age1'))
-  numhs1 = int(request.args.get('numhs1'))
-  SES1 = float(request.args.get('SES1'))
-  frdrop1 = int(request.args.get('frdrop1'))
-  behave1 = float(request.args.get('behave1'))
+  #The age is non-intuituve - correct it
+  if age1 == 15:
+    age1 = 7
+  if age1 == 16:
+    age1 = 6
+  if age1 == 17:
+    age1 = 5
+  if age1 == 18:
+    age1 = 4
+  elif age1 == 19:
+    age1 = 3
+  elif age1 == 20:
+    age1 = 2
+  elif age1 == 21:
+    age1 = 1
+  elif age1 ==22:
+    age1 = 0
+  else: age1 = 23
+  numhs1 = int(request.args.get('numhs'))
+  SES1 = float(request.args.get('SES'))
+  frdrop1 = int(request.args.get('frdrop'))
+  absent1 = float(request.args.get('absent'))
+  susp1 = float(request.args.get('susp'))
+  dropprog1 = (request.args.get('dropprog'))
+  if dropprog1 == 'yes' or dropprog1 == 'Yes' : dropprog1 = 1
+  else: dropprog1 = 0
+  dummy1 = 0 
+  hhnum1 = float(request.args.get('hhnum'))
+  skip1 = float(request.args.get('skip'))
+ 
   #are they ok?
   print request.args.get('ID')
-  print request.args.get('behave1')
-  print request.args.get('frdrop1')
+  print request.args.get('absent')
+  print request.args.get('frdrop')
   print request.args.get('math2')
-  print request.args.get('SES1')
+  print request.args.get('SES')
   print request.args.get('age1')
-  print request.args.get('numhs1')
+  print request.args.get('numhs')
+  print request.args.get('hhnum')
+  print request.args.get('numhs')
 
   IDl.append(ID)
   math2l.append(math2)
   age1l.append(age1)
   numhsl.append(numhs1)
+  hhnuml.append(hhnum1)
   SESl.append(SES1)
   frdropl.append(frdrop1)
-  behavel.append(behave1)
+  dropprogl.append(dropprog1)
+  dummyl.append(dummy1)
+  absentl.append(absent1)
+  suspl.append(susp1)
+  skipl.append(skip1)
 
-  theList = Predict_Final(ID = IDl, math2=math2l, age=age1l, behave=behavel, numhs=numhsl, ses=SESl, frdrop=frdropl)  
-  if request.args.get('math1') < -1.5:
-    theActions.append('Due to low student math scores, intervene with tutoring')
-  else:
-    theActions.append('This student is awesome, go away')
+  # This is the result...
+  theList = Predict_Final(ID = IDl, math2=math2l, age=age1l, absent=absentl, skip=skipl, susp = suspl,  numhs=numhsl, ses=SESl, frdrop=frdropl, dropprog=dropprogl, hhnum=hhnuml, dummy=dummyl)  
+  # Do you want to build the actions here or in the other class?
+  if absent1 > 4:
+    theActions.append('The student is missing many days, investigate why.')
+  if request.args.get('math2') < -1.5:
+    theActions.append('Due to low student math scores, intervene with tutoring.')
   return render_template("result.html", theList=theList, theActions=theActions)
 
